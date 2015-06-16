94af27023c1cff1c1ab326f139d0cf40e03d673d
Stoica Cosmin-Stefan <cosminstefan.stoica@freescale.com>
2015-06-16 11:05:21 +0300

    s32v234evb: Disable console timeout
    Signed-off-by: Stoica Cosmin-Stefan <cosminstefan.stoica@freescale.com>

--- include/configs/s32v234fvb.h
+++ include/configs/s32v234fvb.h
@@ -133,7 +133,7 @@
 #define CONFIG_SYS_64BIT_VSPRINTF  /* needed for nand_util.c */
 #endif
 
-#define CONFIG_BOOTDELAY		(-1)
+#define CONFIG_BOOTDELAY		-1
 
 #define CONFIG_LOADADDR			(DDR_BASE_ADDR + 0x2000000)
 #define CONFIG_BOOTARGS			"console=ttyLF0 root=/dev/ram rw"
